253b46d9c8db581167305a7b0e8e3fb8f3a0b845
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-11 09:43:50 +0200

    Skapar grund struktur för footer

--- footer.php
+++ footer.php
@@ -1,4 +1,26 @@
-<footer>
+<footer id="footer">
+    <div class="container">
+        <div class="row top">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+
+            </div>
+
+            <div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">
+
+            </div>
+
+            <div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">
+
+            </div>
+        </div>
+
+        <div class="row bottom">
+            <div class="col-xs-12">
+                
+            </div>
+        </div>
+
+    </div>
 
 </footer>
 
