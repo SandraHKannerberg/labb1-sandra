ce1d4e5b5a1faafa340003c9b7739eccd31a7bf2
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-12 11:12:11 +0200

    Skapar innehåll i front-page.php

--- front-page.php
+++ front-page.php
@@ -1,3 +1,25 @@
 <?php get_header(); ?>
 
+<main>
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+
+							<div class="hero"> <!--Öppnar hero-->
+
+                                <?php the_post_thumbnail('large'); ?>
+
+								<div class="text">
+									<h1><?php the_title(); ?></h1>
+									<p><?php the_content(); ?></p>
+								</div>
+
+							</div> <!--Stänger hero-->
+						</div>
+					</div> <!--Stänger row-->
+				</div> <!--Stänger container-->
+			</section>
+		</main>
+
 <?php get_footer(); ?>
