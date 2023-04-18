520547a123940205d31e132d99295beb6178011b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-18 09:04:36 +0200

    Justeringar i search.php för att efterlikna den ursprungliga html-koden bättre

--- search.php
+++ search.php
@@ -8,6 +8,10 @@
 
                     <h1><?php echo "Sökresultat för: "; the_search_query(); ?></h1>
 
+                    <div class="searchform-wrap">
+                        <?php get_search_form(); ?>
+					</div>
+
                     <?php if (have_posts()) : while(have_posts()) : the_post(); ?> <!--Start Loop-->
 
                         <?php get_template_part('template-parts/article-excerpt', 'article-excerpt'); ?>
