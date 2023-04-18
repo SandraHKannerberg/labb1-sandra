561878ca7918b4a6e919d11bea94427de3df8130
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-18 09:08:55 +0200

    Skapar hur innehåll ska visas för index.php

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 		<div class="container">
 			<div class="row"> 
 
+				<?php if (have_posts()) : while(have_posts()) : the_post(); ?> <!--Start Loop-->
+
+					<h1><?php the_title(); ?></h1>
+					<p><?php the_content(); ?></p>
+
+				<?php endwhile; endif; ?> <!--Slut Loop-->
+
             </div> <!--Stänger div row-->
 		</div> <!--Stänger div container-->
 	</section>
